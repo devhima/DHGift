3a2fc7c1420e86838ab8a8f0aedf69ea212a68c6
devhima <>
2025-01-13 04:52:21 +0200

    Last Sync: 2025-01-13 02:52:21 (Mobile)

---
+++ Client/app/src/main/java/com/dh/gift/client/receivers/ServiceTerminate.java
@@ -0,0 +1,15 @@
+package com.dh.gift.client.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.dh.gift.client.ForegroundService;
+import com.dh.gift.client.MainService;
+
+public class ServiceTerminate extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, ForegroundService.class));
+    }
+}
